060fd2840803c62342dbacde48f6f0611a4e9f70
sheenazien8 <sheenazien08@gmail.com>
2024-01-11 11:26:52 +0700

    show the modal setting

--- lib/screens/setting/setting_screen.dart
+++ lib/screens/setting/setting_screen.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:lakasir/utils/colors.dart';
+import 'package:lakasir/widgets/dialog.dart';
+import 'package:lakasir/widgets/filled_button.dart';
 import 'package:lakasir/widgets/layout.dart';
 import 'package:lakasir/widgets/my_card_list.dart';
+import 'package:lakasir/widgets/select_input_feld.dart';
+import 'package:lakasir/widgets/text_field.dart';
 
 class SettingScreen extends StatelessWidget {
   const SettingScreen({super.key});
@@ -41,7 +46,33 @@ class SettingScreen extends StatelessWidget {
               ),
               const SizedBox(height: 19),
               MyCardList(
-                route: "/menu/setting/currency",
+                onTap: () {
+                  var currencyController = SelectInputWidgetController();
+                  currencyController.selectedOption = "IDR";
+                  Get.dialog(MyDialog(
+                    content: Column(
+                      children: [
+                        SelectInputWidget(
+                          controller: currencyController,
+                          label: "Currency",
+                          options: [
+                            Option(
+                              name: "IDR",
+                              value: "IDR",
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        MyFilledButton(
+                          onPressed: () {
+                            Get.back();
+                          },
+                          child: const Text("Save"),
+                        ),
+                      ],
+                    ),
+                  ));
+                },
                 imagebox: Container(
                   width: 52,
                   height: 52,
@@ -65,6 +96,33 @@ class SettingScreen extends StatelessWidget {
               const Text("System", style: TextStyle(fontSize: 20)),
               const SizedBox(height: 19),
               MyCardList(
+                onTap: () {
+                  var languageController = SelectInputWidgetController();
+                  languageController.selectedOption = "en";
+                  Get.dialog(MyDialog(
+                    content: Column(
+                      children: [
+                        SelectInputWidget(
+                          controller: languageController,
+                          label: "Language",
+                          options: [
+                            Option(
+                              name: "English",
+                              value: "en",
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        MyFilledButton(
+                          onPressed: () {
+                            Get.back();
+                          },
+                          child: const Text("Save"),
+                        ),
+                      ],
+                    ),
+                  ));
+                },
                 imagebox: Container(
                   width: 52,
                   height: 52,
@@ -86,6 +144,20 @@ class SettingScreen extends StatelessWidget {
               ),
               const SizedBox(height: 19),
               MyCardList(
+                onTap: () {
+                  Get.dialog(AlertDialog(
+                    title: const Text("Dark Mode"),
+                    content: const Text("Coming Soon"),
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          Get.back();
+                        },
+                        child: const Text("OK"),
+                      ),
+                    ],
+                  ));
+                },
                 imagebox: Container(
                   width: 52,
                   height: 52,
