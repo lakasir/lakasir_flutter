cb112670dbaa1fb5bafe16194b52fc445e283c92
sheenazien8 <sheenazien08@gmail.com>
2026-04-26 12:46:37 +0700

    feat(offline): refactor PaymentController to use TransactionRepository
    - PaymentController now uses TransactionRepository instead of PaymentSerivce
    - procceedThePayment() returns OfflinePendingTransaction stored in lastTransaction
    - memberId uses remoteId (server ID) if available, falls back to local ID
    - Removed unused HistoryController dependency
    - Removed unused imports (print_receipt, utils, pagination_request)
    - Transaction flow: online mode tries API then caches locally, offline mode saves directly

--- lib/controllers/transactions/payment_controller.dart
+++ lib/controllers/transactions/payment_controller.dart
@@ -3,32 +3,26 @@ import 'dart:convert';
 import 'package:blue_thermal_printer/blue_thermal_printer.dart';
 import 'package:get/get.dart';
 import 'package:lakasir/Exceptions/validation.dart';
-import 'package:lakasir/api/requests/pagination_request.dart';
 import 'package:lakasir/api/requests/payment_request.dart';
 import 'package:lakasir/api/responses/error_response.dart';
-import 'package:lakasir/api/responses/transactions/history_response.dart';
-import 'package:lakasir/controllers/products/product_controller.dart';
-import 'package:lakasir/controllers/settings/print_controller.dart';
 import 'package:lakasir/controllers/transactions/cart_controller.dart';
-import 'package:lakasir/controllers/transactions/history_controller.dart';
 import 'package:lakasir/models/printer.dart';
-import 'package:lakasir/screens/setting/printers/print_receipt.dart';
-import 'package:lakasir/services/payment_service.dart';
+import 'package:lakasir/offline/models/pending_transaction_model.dart';
+import 'package:lakasir/offline/repositories/transaction_repository.dart';
 import 'package:lakasir/utils/colors.dart';
-import 'package:lakasir/utils/utils.dart';
 
 class PaymentController extends GetxController {
-  final _paymentService = PaymentSerivce();
+  final _transactionRepository = TransactionRepository();
   final _cartController = Get.put(CartController());
-  final _transactionController = Get.put(HistoryController());
   final RxBool isLoading = false.obs;
+  final Rx<OfflinePendingTransaction?> lastTransaction = Rx<OfflinePendingTransaction?>(null);
 
   void store() async {
     try {
       var change = _cartController.cartSessions.value.payedMoney! -
           _cartController.cartSessions.value.getTotalPrice;
       isLoading(true);
-      procceedThePayment();
+      await procceedThePayment();
       Get.offAllNamed("/auth");
       Get.toNamed("/menu/transaction/cashier");
       Get.toNamed(
@@ -49,14 +43,14 @@ class PaymentController extends GetxController {
     }
   }
 
-  void procceedThePayment() async {
-    TransactionHistoryResponse transactionResponse =
-        await _paymentService.store(PaymentRequest(
+  Future<void> procceedThePayment() async {
+    final transaction = await _transactionRepository.store(PaymentRequest(
       discountPrice: _cartController.cartSessions.value.discountPrice,
       payedMoney: _cartController.cartSessions.value.payedMoney,
       friendPrice: false,
       tax: _cartController.cartSessions.value.tax,
-      memberId: _cartController.cartSessions.value.member?.id,
+      memberId: _cartController.cartSessions.value.member?.remoteId ??
+          _cartController.cartSessions.value.member?.id,
       note: _cartController.cartSessions.value.note,
       products: _cartController.cartSessions.value.cartItems
           .map(
@@ -69,9 +63,7 @@ class PaymentController extends GetxController {
           .toList(),
     ));
 
-    debug(transactionResponse.cashier!.name!);
-
-    _transactionController.transaction(transactionResponse);
+    lastTransaction.value = transaction;
   }
 
   void printReceipt(BlueThermalPrinter bluetooth, Printer printer) {}
